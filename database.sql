e66c54f53a97d3099bfe2e9d3435341cf6d2adfd
changngoc29 <vtkienn@gmail.com>
2022-12-03 16:04:41 +0700

    update database

--- database.sql
+++ database.sql
@@ -20,4 +20,34 @@ values (1, 'admin name', 'male', '0914221133', 'admin', '123456', 'admin', '');
 insert into user
 values (null, 'user name', 'male', '0914221133', 'user', '123456', 'user', 'dev');
 
-select * from user
+insert into user
+values (null, 'user2 name', 'male', '0914221133', 'user2', '123456', 'user2', 'dev');
+
+insert into user
+values (null, 'user3 name', 'male', '0914221133', 'user3', '123456', 'user3', 'dev');
+
+create table task(
+	id int auto_increment,
+    title varchar(50),
+    description varchar(100),
+    staff_id int,
+    start_date date,
+    deadline date,
+    status varchar(10),
+    dep varchar(5),
+    primary key (id),
+    foreign key (staff_id) references user(id)
+);
+
+insert into task
+values (1, 'this is a test title', 'this is also a test description', 2, '2022-11-30', '2022-11-5', 'pending', 'dev');
+
+insert into task
+values (null, 'this is a test title 2', 'this is also a test description 2', 3, '2022-11-30', '2022-11-5', 'pending', 'dev');
+
+insert into task
+values (null, 'this is a test title 3', 'this is also a test description 3', 3, '2022-11-30', '2022-11-5', 'pending', 'dev');
+
+
+
+
