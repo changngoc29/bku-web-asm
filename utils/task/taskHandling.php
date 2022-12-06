2caa7d475d400e7f66f6e366b07af36022143604
changngoc29 <vtkienn@gmail.com>
2022-12-06 21:13:53 +0700

    update task

--- utils/task/taskHandling.php
+++ utils/task/taskHandling.php
@@ -4,13 +4,13 @@ session_start();
 
 if (isset($_GET) && $_GET["action"] == 'create') {
     global $conn;
-    echo $_POST['title'];
-    echo $_POST['desc'];
-    echo $_POST['staff-id'];
-    echo $_POST['deadline'];
 
     $title = mysqli_real_escape_string($conn, $_POST['title']);
     $desc = mysqli_real_escape_string($conn, $_POST['desc']);
     $staffID = mysqli_real_escape_string($conn, $_POST['staff-id']);
     $deadline = mysqli_real_escape_string($conn, $_POST['deadline']);
+
+    $query = "INSERT INTO task(title, description, staff_id, start_date, deadline, status, dep) VALUES('$title', '$desc', '$staffID', CURDATE(), '$deadline', 'pending', '{$_SESSION['user_dep']}')";
+    mysqli_query($conn, $query);
+    header("Location: ../../pages/task.php");
 }
